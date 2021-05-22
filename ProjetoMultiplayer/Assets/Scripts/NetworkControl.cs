fccc4caedced4d27ba133100299945b44aaa55ee
johookgjiot <gfdl.joao@gmail.com>
2021-05-21 22:47:47 -0300

    Login Com Photon 21/05/2021

---
+++ ProjetoMultiplayer/Assets/Scripts/NetworkControl.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class NetworkControl : MonoBehaviourPunCallbacks
+{
+    [Header("LOGIN")]
+    public GameObject loginPn;
+    public InputField playerNameInput;
+    string tempPlayerName;
+
+    [Header("LOBBY")]
+    public GameObject lobbyPn;
+    public InputField roomNameInput;
+    string tempRoomName;
+
+    [Header("PLAYER")]
+    public GameObject playerPUN;
+    public GameObject mainCamera;
+
+    [Header("Spawnpoints")]
+    public GameObject SpawnA;
+    public GameObject SpawnB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loginPn.gameObject.SetActive(true);
+        lobbyPn.gameObject.SetActive(false);
+
+        tempPlayerName = "Usuário" + Random.Range(8, 99);
+        playerNameInput.text = tempPlayerName;
+
+        tempRoomName = "Sala" + Random.Range(8, 99);
+    }
+
+
+    public void Login()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+
+        if (playerNameInput.text != "")
+        {
+            PhotonNetwork.NickName = playerNameInput.text;
+        }
+        else
+        {
+            PhotonNetwork.NickName = tempPlayerName;
+        }
+
+        loginPn.gameObject.SetActive(false);
+        lobbyPn.gameObject.SetActive(true);
+        roomNameInput.text = tempRoomName;
+    }
+
+    public void QuickSearch()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void CreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions() { MaxPlayers = 4 };
+        PhotonNetwork.JoinOrCreateRoom(roomNameInput.text, roomOptions, TypedLobby.Default);
+    }
+
+
+
+    //PUN
+    //############# PUN Callbacks ##################
+    public override void OnConnected()
+    {
+        Debug.LogWarning("############# LOGIN #############");
+        Debug.LogWarning("OnConnected");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.LogWarning("OnConnectedToMaster");
+        Debug.LogWarning("Server: " + PhotonNetwork.CloudRegion);
+        Debug.LogWarning("Ping: " + PhotonNetwork.GetPing());
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.LogWarning("############# LOOBY #############");
+        Debug.LogWarning("OnJoinedLobby");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(tempRoomName);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.LogWarning("OnJoinedRoom");
+        Debug.LogWarning("Nome da Sala: " + PhotonNetwork.CurrentRoom.Name);
+        Debug.LogWarning("Nome da Player: " + PhotonNetwork.NickName);
+        Debug.LogWarning("Players Conectados: " + PhotonNetwork.CurrentRoom.PlayerCount);
+
+        loginPn.gameObject.SetActive(false);
+        lobbyPn.gameObject.SetActive(false);
+        mainCamera.gameObject.SetActive(false);
+
+        Vector3 pos = SpawnA.transform.position;
+
+        PhotonNetwork.Instantiate(playerPUN.name, pos, playerPUN.transform.rotation, 0);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
