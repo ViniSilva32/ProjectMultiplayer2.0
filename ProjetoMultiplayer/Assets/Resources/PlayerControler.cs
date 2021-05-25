b733fada7bf703f522364abe7ce3de5673318048
BigRodri <rodrigonnes@icloud.com>
2021-05-24 22:24:11 -0300

    Att
    Script de movimentação do player + animação idle

---
+++ ProjetoMultiplayer/Assets/Resources/PlayerControler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float velocidade;
+    public float girar;
+    private Animator anim;
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocidade = 20.0f;
+        girar = 60.0f;
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        //Pulo();//
+        
+    }
+
+    void Move()
+    {
+        float translate = (Input.GetAxis("Vertical") * velocidade) * Time.deltaTime;
+        float rotate = (Input.GetAxis("Horizontal") * girar) * Time.deltaTime;
+
+        transform.Translate(0, 0, translate);
+        transform.Rotate(0, rotate, 0);
+
+        
+    }
+     void Pulo()
+    {
+        
+    }
+}
